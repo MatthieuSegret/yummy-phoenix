d1946a11a76ee1cedbfd1e6edd0a7d43ab254660
Matthieu Segret <matthieu.segret@gmail.com>
2017-08-25 14:33:25 +0200

    Use markdown to recipe page

--- lib/yummy_web/views/recipe_view.ex
+++ lib/yummy_web/views/recipe_view.ex
@@ -1,3 +1,22 @@
 defmodule YummyWeb.RecipeView do
   use YummyWeb, :view
+
+  def description(text) do
+    text
+    |> truncate(length: 180)
+    |> String.replace(~r/\r|\n|\t/, " ")
+    |> String.replace(~r/#|\*/, "")
+  end
+
+  def markdown(text) do
+    text
+    |> Earmark.as_html!
+    |> raw
+  end
+
+  defp truncate(text, length: length) do
+    text
+    |> String.slice(1..length)
+    |> String.replace(~r/\s(.)*$/, "...")
+  end
 end
